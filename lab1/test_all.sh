65b8410d062eabda6c0ee8301efbb1864fd65d6f
Daniel Vyenielo <dvyen001@bolt.cs.ucr.edu>
2020-01-24 14:52:40 -0800

    The real test scripts.

---
+++ lab1/test_all.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+min_files=(./min/*)
+token_files=(./tokens/*)
+
+for i in {0..2}
+do
+	echo "###############################################"
+	echo "${min_files[$i]}" "${token_files[$i]}"  
+	./test.sh "${min_files[$i]}" "${token_files[$i]}"  
+done ;
