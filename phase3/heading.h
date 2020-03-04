50c9be1615ca29bdbaa6e61be29c204c9054f26e
Dan Vyenielo <dvyenielo@gmail.com>
2020-03-03 18:15:20 -0800

    Simplify our lives.

--- phase3/heading.h
+++ phase3/heading.h
@@ -3,10 +3,12 @@
 #define YY_NO_UNPUT
 
 using namespace std;
-
+#include <bits/stdc++.h>
+/*
 #include <iostream>
 #include <cstring>
 #include <cstdio>
 #include <cstdlib>
 #include <string>
-
+#include <vector>
+*/
